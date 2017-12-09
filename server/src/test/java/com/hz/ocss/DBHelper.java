6cb72468660f42a268ca4704773d7c8dd4bf484b
Xiaozh <838223935@qq.com>
2017-12-09 15:15:45 +0800

    [add] init project

---
+++ server/src/test/java/com/hz/ocss/DBHelper.java
@@ -0,0 +1,23 @@
+package com.hz.ocss;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+//@SpringBootConfiguration
+public class DBHelper {
+//    private static EmbeddedDatabase db;
+//    static {
+//        db = new EmbeddedDatabaseBuilder().generateUniqueName(true)
+//                .setScriptEncoding("UTF-8")
+//                .ignoreFailedDrops(true)
+//                .addScript("classpath:schema.sql").build();
+//    }
+
+
+
+//    public static EmbeddedDatabase getDB() {
+//        return db;
+//    }
+}
---
+++ server/src/test/java/com/hz/ocss/controller/UserTests.java
@@ -0,0 +1,41 @@
+package com.hz.ocss.controller;
+
+import com.hz.ocss.DBHelper;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Component
+public class UserTests {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+//        DBHelper.getDB();
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "dept");
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/user/login.do").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andExpect(content().string(equalTo("hello")));
+    }
+}
