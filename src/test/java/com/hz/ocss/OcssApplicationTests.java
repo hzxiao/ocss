332902c58cc637239c96f587964ae5fd59f64eee
Xiaozh <838223935@qq.com>
2017-12-09 18:50:34 +0800

    [reset] project

--- src/test/java/com/hz/ocss/OcssApplicationTests.java
+++ src/test/java/com/hz/ocss/OcssApplicationTests.java
@@ -2,20 +2,15 @@ package com.hz.ocss;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Sql(scripts = "classpath:schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class OcssApplicationTests {
 
 	@Test
 	public void contextLoads() {
-
 	}
+
 }
